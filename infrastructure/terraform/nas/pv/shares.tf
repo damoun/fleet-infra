e004e1f35247295bfea5f509d9d11093f74758c7
Damien Plénard <damien@plenard.me>
2023-12-06 22:45:35 +0100

    remove NAS PV module for kubernetes volumes
    Signed-off-by: Damien Plénard <damien@plenard.me>

--- infrastructure/terraform/nas/pv/shares.tf
+++
@@ -1,8 +0,0 @@
-resource "truenas_share_nfs" "share" {
-  for_each     = truenas_dataset.volume
-  enabled      = true
-  paths        = [each.value.mount_point]
-  mapall_user  = "nobody"
-  mapall_group = "nobody"
-  comment      = "Created by terraform"
-}
