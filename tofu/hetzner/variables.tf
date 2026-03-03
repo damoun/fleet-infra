39956e544d6b60d5ce1ca9eda35d4beba2a33002
Damien Plénard <damien@plenard.me>
2026-03-04 00:45:14 +0100

    feat: add OpenTofu variables for Hetzner cluster

---
+++ tofu/hetzner/variables.tf
@@ -0,0 +1,64 @@
+variable "hcloud_token" {
+  type      = string
+  sensitive = true
+}
+
+variable "state_passphrase" {
+  type      = string
+  sensitive = true
+}
+
+variable "cluster_name" {
+  type    = string
+  default = "dapl-prd-htz-fsn-app"
+}
+
+variable "location" {
+  type    = string
+  default = "fsn1"
+}
+
+variable "talos_version" {
+  type    = string
+  default = "v1.12.4"
+}
+
+variable "server_type" {
+  type    = string
+  default = "cx22"
+}
+
+variable "control_plane_count" {
+  type    = number
+  default = 3
+}
+
+variable "worker_count" {
+  type    = number
+  default = 3
+}
+
+variable "admin_cidrs" {
+  type        = list(string)
+  description = "CIDRs allowed to reach K8s API and Talos endpoints"
+}
+
+variable "network_cidr" {
+  type    = string
+  default = "172.16.16.0/20"
+}
+
+variable "node_subnet_cidr" {
+  type    = string
+  default = "172.16.16.0/24"
+}
+
+variable "pod_cidr" {
+  type    = string
+  default = "172.20.0.0/16"
+}
+
+variable "service_cidr" {
+  type    = string
+  default = "172.24.0.0/16"
+}
