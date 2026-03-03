c5d4d5fc4cbc824332d5edd5cfbd771f3cfc3d47
Damien Plénard <damien@plenard.me>
2026-03-04 00:50:09 +0100

    feat: add OpenTofu outputs for kubeconfig, talosconfig, IPs

---
+++ tofu/hetzner/outputs.tf
@@ -0,0 +1,21 @@
+output "kubeconfig" {
+  value     = data.talos_cluster_kubeconfig.this.kubeconfig_raw
+  sensitive = true
+}
+
+output "talosconfig" {
+  value     = data.talos_client_configuration.this.talos_config
+  sensitive = true
+}
+
+output "load_balancer_ip" {
+  value = hcloud_load_balancer.api.ipv4
+}
+
+output "control_plane_ips" {
+  value = [for s in hcloud_server.control_plane : s.ipv4_address]
+}
+
+output "worker_ips" {
+  value = [for s in hcloud_server.worker : s.ipv4_address]
+}
