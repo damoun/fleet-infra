61be2298e15dc56afbb2a20056a49e18d72ca62d
Damien Plénard <damien@plenard.me>
2026-03-04 00:49:14 +0100

    feat: add control plane and worker server definitions

---
+++ tofu/hetzner/servers.tf
@@ -0,0 +1,47 @@
+data "hcloud_image" "talos" {
+  with_selector     = "type=talos,version=${var.talos_version}"
+  most_recent       = true
+  with_architecture = "x86"
+}
+
+resource "hcloud_server" "control_plane" {
+  count              = var.control_plane_count
+  name               = "${var.cluster_name}-cp-${count.index}"
+  server_type        = var.server_type
+  location           = var.location
+  image              = data.hcloud_image.talos.id
+  placement_group_id = hcloud_placement_group.control_plane.id
+  firewall_ids       = [hcloud_firewall.cluster.id]
+
+  network {
+    network_id = hcloud_network.cluster.id
+  }
+
+  depends_on = [hcloud_network_subnet.nodes]
+
+  labels = {
+    cluster = var.cluster_name
+    role    = "control-plane"
+  }
+}
+
+resource "hcloud_server" "worker" {
+  count              = var.worker_count
+  name               = "${var.cluster_name}-worker-${count.index}"
+  server_type        = var.server_type
+  location           = var.location
+  image              = data.hcloud_image.talos.id
+  placement_group_id = hcloud_placement_group.worker.id
+  firewall_ids       = [hcloud_firewall.cluster.id]
+
+  network {
+    network_id = hcloud_network.cluster.id
+  }
+
+  depends_on = [hcloud_network_subnet.nodes]
+
+  labels = {
+    cluster = var.cluster_name
+    role    = "worker"
+  }
+}
