0c12081c9b89338ebff279ab16b35d54ee3d03c4
Damien Plénard <damien@plenard.me>
2026-03-04 00:49:47 +0100

    feat: add Talos machine config, bootstrap, and kubeconfig

---
+++ tofu/hetzner/talos.tf
@@ -0,0 +1,95 @@
+resource "talos_machine_secrets" "this" {}
+
+data "talos_machine_configuration" "control_plane" {
+  cluster_name     = var.cluster_name
+  machine_type     = "controlplane"
+  cluster_endpoint = "https://${hcloud_load_balancer.api.ipv4}:6443"
+  machine_secrets  = talos_machine_secrets.this.machine_secrets
+
+  config_patches = [
+    yamlencode({
+      cluster = {
+        network = {
+          cni = { name = "none" }
+          podSubnets     = [var.pod_cidr]
+          serviceSubnets = [var.service_cidr]
+        }
+        proxy = { disabled = true }
+      }
+      machine = {
+        features = {
+          kubePrism = {
+            enabled = true
+            port    = 7445
+          }
+        }
+      }
+    }),
+  ]
+}
+
+data "talos_machine_configuration" "worker" {
+  cluster_name     = var.cluster_name
+  machine_type     = "worker"
+  cluster_endpoint = "https://${hcloud_load_balancer.api.ipv4}:6443"
+  machine_secrets  = talos_machine_secrets.this.machine_secrets
+
+  config_patches = [
+    yamlencode({
+      cluster = {
+        network = {
+          cni = { name = "none" }
+          podSubnets     = [var.pod_cidr]
+          serviceSubnets = [var.service_cidr]
+        }
+        proxy = { disabled = true }
+      }
+      machine = {
+        features = {
+          kubePrism = {
+            enabled = true
+            port    = 7445
+          }
+        }
+      }
+    }),
+  ]
+}
+
+resource "talos_machine_configuration_apply" "control_plane" {
+  count                       = var.control_plane_count
+  client_configuration        = talos_machine_secrets.this.client_configuration
+  machine_configuration_input = data.talos_machine_configuration.control_plane.machine_configuration
+  node                        = hcloud_server.control_plane[count.index].ipv4_address
+}
+
+resource "talos_machine_configuration_apply" "worker" {
+  count                       = var.worker_count
+  client_configuration        = talos_machine_secrets.this.client_configuration
+  machine_configuration_input = data.talos_machine_configuration.worker.machine_configuration
+  node                        = hcloud_server.worker[count.index].ipv4_address
+}
+
+resource "talos_machine_bootstrap" "this" {
+  client_configuration = talos_machine_secrets.this.client_configuration
+  node                 = hcloud_server.control_plane[0].ipv4_address
+
+  depends_on = [talos_machine_configuration_apply.control_plane]
+}
+
+data "talos_cluster_kubeconfig" "this" {
+  client_configuration = talos_machine_secrets.this.client_configuration
+  node                 = hcloud_server.control_plane[0].ipv4_address
+
+  depends_on = [talos_machine_bootstrap.this]
+}
+
+data "talos_client_configuration" "this" {
+  cluster_name         = var.cluster_name
+  client_configuration = talos_machine_secrets.this.client_configuration
+  nodes                = concat(
+    [for s in hcloud_server.control_plane : s.ipv4_address],
+    [for s in hcloud_server.worker : s.ipv4_address],
+  )
+  endpoints = [for s in hcloud_server.control_plane : s.ipv4_address]
+}
