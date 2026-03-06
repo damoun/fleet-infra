6a09fce6f32a7a2e49dd21cba6f72ec9411390c1
Damien Plénard <damien@plenard.me>
2026-03-06 09:55:28 +0100

    feat: remove kube API firewall rule, keep talosctl only

--- tofu/hetzner/firewall.tf
+++ tofu/hetzner/firewall.tf
@@ -1,14 +1,6 @@
 resource "hcloud_firewall" "cluster" {
   name = var.cluster_name
 
-  # Kubernetes API
-  rule {
-    direction  = "in"
-    protocol   = "tcp"
-    port       = "6443"
-    source_ips = var.admin_cidrs
-  }
-
   # Talos apid
   rule {
     direction  = "in"
