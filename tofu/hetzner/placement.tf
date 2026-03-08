e518fa59968732ccc5fcad06b8a99ca856f81443
Damien Plénard <damien@plenard.me>
2026-03-08 13:56:46 +0100

    feat: replace hetzner tofu code with terraform-hcloud-talos module call

--- tofu/hetzner/placement.tf
+++
@@ -1,9 +0,0 @@
-resource "hcloud_placement_group" "control_plane" {
-  name = "${var.cluster_name}-cp"
-  type = "spread"
-}
-
-resource "hcloud_placement_group" "worker" {
-  name = "${var.cluster_name}-worker"
-  type = "spread"
-}
