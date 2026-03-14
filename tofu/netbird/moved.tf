5d83544fde813000fb5496d4bd2cca53af2af135
Damien Plénard <damien@plenard.me>
2026-03-14 23:02:50 +0100

    refactor: remove moved blocks after apply

--- tofu/netbird/moved.tf
+++
@@ -1,9 +0,0 @@
-moved {
-  from = netbird_group.kubernetes_admin
-  to   = netbird_group.kubernetes
-}
-
-moved {
-  from = netbird_policy.kubernetes_admin
-  to   = netbird_policy.admin_to_kubernetes
-}
